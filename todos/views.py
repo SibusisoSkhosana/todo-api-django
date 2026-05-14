7edbb4f01e006eed5209f81c7724a864803a2d24
Sibusiso D. Skhosana <sbasiso@gmail.com>
2026-05-15 01:22:18 +0200

    Dockerfile and docker-compose setup

--- todos/views.py
+++ todos/views.py
@@ -1,12 +1,26 @@
-from django.shortcuts import render
+import logging
 from rest_framework import generics
 from .models import Todo
 from .serializers import TodoSerializer
+
+logger = logging.getLogger(__name__)
 
 class TodoListCreateView(generics.ListCreateAPIView):
     queryset = Todo.objects.all().order_by("-created_at")
     serializer_class = TodoSerializer
 
+    def perform_create(self, serializer):
+        todo = serializer.save()
+        logger.info(f"Todo created: {todo.id} - {todo.title}")
+
 class TodoDetailView(generics.RetrieveUpdateDestroyAPIView):
     queryset = Todo.objects.all()
     serializer_class = TodoSerializer
+
+    def perform_update(self, serializer):
+        todo = serializer.save()
+        logger.info(f"Todo updated: {todo.id}")
+
+    def perform_destroy(self, instance):
+        logger.info(f"Todo deleted: {instance.id}")
+        instance.delete()
