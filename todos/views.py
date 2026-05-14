e84606bdae10a9e120bc52d1571e8ee2c8714929
Sibusiso D. Skhosana <sbasiso@gmail.com>
2026-05-14 22:47:28 +0200

    Implemented API endpoints and passing unittests

--- todos/views.py
+++ todos/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from rest_framework import generics
+from .models import Todo
+from .serializers import TodoSerializer
 
-# Create your views here.
+class TodoListCreateView(generics.ListCreateAPIView):
+    queryset = Todo.objects.all().order_by("-created_at")
+    serializer_class = TodoSerializer
+
+class TodoDetailView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Todo.objects.all()
+    serializer_class = TodoSerializer
