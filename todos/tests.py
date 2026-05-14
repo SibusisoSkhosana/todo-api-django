67f1538aaf8bdcd36a7aee388522df7be1913076
Sibusiso D. Skhosana <sbasiso@gmail.com>
2026-05-14 22:27:14 +0200

    Added test_delete and test_empty_validation

--- todos/tests.py
+++ todos/tests.py
@@ -45,3 +45,18 @@ class TodoApiTests(APITestCase):
         response = self.client.put(f"/api/todos/{self.todo.id}/", data)
 
         self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_delete_todo(self):
+        response = self.client.delete(f"/api/todos/{self.todo.id}/")
+
+        self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
+
+    def test_empty_title_validation(self):
+        data = {
+            "title": "   ",
+            "description": "Invalid"
+        }
+
+        response = self.client.post("/api/todos/", data)
+
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
